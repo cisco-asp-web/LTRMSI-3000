088d657f28920f7ae24c0b7710d3f8dd5d7dbcbf
brmcdoug <brmcdoug@cisco.com>
2025-05-03 22:58:22 -0700

    script

--- lab_1/scripts/container-ips.sh
+++ lab_1/scripts/container-ips.sh
@@ -8,7 +8,8 @@ docker exec -it clab-clus25-amsterdam ip route add 10.107.1.0/24 via 10.101.1.1 
 docker exec -it clab-clus25-amsterdam ip route add 20.0.0.0/24 via 10.101.1.1 dev eth1
 docker exec -it clab-clus25-amsterdam ip route add 30.0.0.0/24 via 10.101.1.1 dev eth1
 docker exec -it clab-clus25-amsterdam ip -6 route add fc00:0000::/32 via fc00:0:101:1::1 dev eth1
-echo "Amsterdam eth1"
+echo "
+Amsterdam eth1"
 docker exec -it clab-clus25-amsterdam ip addr show eth1 | grep inet
 
 # Amsterdam eth2 - vrf carrots
@@ -20,10 +21,12 @@ docker exec -it clab-clus25-amsterdam ip route add 40.0.0.0/24 via 10.101.2.1
 docker exec -it clab-clus25-amsterdam ip route add 50.0.0.0/24 via 10.101.2.1
 docker exec -it clab-clus25-amsterdam ip -6 route add fc00:0000:40::/64 via fc00:0:101:2::1 dev eth2
 docker exec -it clab-clus25-amsterdam ip -6 route add fc00:0000:50::/64 via fc00:0:101:2::1 dev eth2
-echo "Amsterdam eth2"
+echo "
+Amsterdam eth2"
 docker exec -it clab-clus25-amsterdam ip addr show eth2 | grep inet
 
-echo "Amsterdam routes"
+echo "
+Amsterdam routes"
 docker exec -it clab-clus25-amsterdam ip route
 
 # Rome eth1 - global table
@@ -32,7 +35,8 @@ docker exec -it clab-clus25-rome ip addr add 10.107.1.2/24 dev eth1
 docker exec -it clab-clus25-rome ip addr add fc00:0:107:1::2/64 dev eth1
 docker exec -it clab-clus25-rome ip route add 10.101.1.0/24 via 10.107.1.1 dev eth1
 docker exec -it clab-clus25-rome ip -6 route add fc00:0000::/32 via fc00:0:107:1::1 dev eth1
-echo "Rome eth1"
+echo "
+Rome eth1"
 docker exec -it clab-clus25-rome ip addr show eth1 | grep inet
 
 # Rome eth2 - vrf carrots
@@ -42,10 +46,12 @@ docker exec -it clab-clus25-rome ip addr add fc00:0:107:2::2/64 dev eth2
 docker exec -it clab-clus25-rome ip route add 10.101.2.0/24 via 10.107.2.1 dev eth2
 docker exec -it clab-clus25-rome ip -6 route add fc00:0000:40::/64 via fc00:0:107:2::1 dev eth2
 docker exec -it clab-clus25-rome ip -6 route add fc00:0000:50::/64 via fc00:0:107:2::1 dev eth2
-echo "Rome eth2"
+echo "
+Rome eth2"
 docker exec -it clab-clus25-rome ip addr show eth2 | grep inet
 
-echo "Rome routes"
+echo "
+Rome routes"
 docker exec -it clab-clus25-rome ip route
 
 # Rome loopback interface
@@ -55,4 +61,7 @@ docker exec -it clab-clus25-rome ip addr add 30.0.0.1/24 dev lo
 docker exec -it clab-clus25-rome ip addr add 40.0.0.1/24 dev lo
 docker exec -it clab-clus25-rome ip addr add 50.0.0.1/24 dev lo
 
+echo "
+Rome loopback"
+docker exec -it clab-clus25-rome ip addr show lo | grep inet
 
