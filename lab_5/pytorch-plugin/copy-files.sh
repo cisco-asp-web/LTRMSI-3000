e2aae4a2ff7f9eefb3c9749eece5e9ff9c0c6471
brmcdoug <brmcdoug@cisco.com>
2025-07-03 15:58:12 -0700

    new sonic img

--- lab_5/pytorch-plugin/copy-files.sh
+++ lab_5/pytorch-plugin/copy-files.sh
@@ -47,3 +47,18 @@ docker cp srv6_plugin.py clab-clus25-host03:/app/
 docker cp dist_setup.py clab-clus25-host03:/app/
 docker cp controller.py clab-clus25-host03:/app/
 docker cp route_programmer.py clab-clus25-host03:/app/
+
+
+docker cp demo/.env clab-clus25-amsterdam-host00:/app/
+docker cp demo/test_plugin.py clab-clus25-amsterdam-host00:/app/
+docker cp srv6_plugin.py clab-clus25-amsterdam-host00:/app/
+docker cp dist_setup.py clab-clus25-amsterdam-host00:/app/
+docker cp controller.py clab-clus25-amsterdam-host00:/app/
+docker cp route_programmer.py clab-clus25-amsterdam-host00:/app/
+
+docker cp demo/.env clab-clus25-rome-host00:/app/
+docker cp demo/test_plugin.py clab-clus25-rome-host00:/app/
+docker cp srv6_plugin.py clab-clus25-rome-host00:/app/
+docker cp dist_setup.py clab-clus25-rome-host00:/app/
+docker cp controller.py clab-clus25-rome-host00:/app/
+docker cp route_programmer.py clab-clus25-rome-host00:/app/
