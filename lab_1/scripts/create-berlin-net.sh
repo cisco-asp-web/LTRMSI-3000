2a198dddeb177930daa3b8733f4e45155561e54f
brmcdoug <brmcdoug@cisco.com>
2025-08-08 20:25:03 -0700

    warsaw net

--- lab_1/scripts/create-berlin-net.sh
+++ lab_1/scripts/create-berlin-net.sh
@@ -2,13 +2,18 @@
 
 # Exit if already exists
 ip link show berlin-net &>/dev/null && exit 0
+ip link show warsaw-net &>/dev/null && exit 0
 
 # Create the bridge
 ip link add name berlin-net type bridge
 ip link set dev berlin-net up
 
+ip link add name warsaw-net type bridge
+ip link set dev warsaw-net up
+
 # Add the IP address to the bridge for Berlin VMs default route
 ip addr add 198.18.4.3/24 dev berlin-net
+ip addr add 198.18.5.3/24 dev warsaw-net
 
-# Add the iptables rule to SNAT Berlin outbound traffic to the Internet
+# Add the iptables rule to SNAT Berlin and Warsaw outbound traffic to the Internet
 sudo iptables -t nat -A POSTROUTING -o ens160 -j MASQUERADE
